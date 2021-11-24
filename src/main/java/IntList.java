eb3ce373acfdad93dc043674e79eee4d001f1303
Tinyang <1251307117@qq.com>
2021-11-24 16:32:40 +0800

    lesson2.1

---
+++ src/main/java/IntList.java
@@ -0,0 +1,47 @@
+public class IntList {
+    public int first;
+    public IntList rest;
+
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
+
+    public int size() {
+        if (rest == null) {return 1;}
+        return 1 + rest.size();
+    }
+
+    public int iterativeSize() {
+        IntList temp = this.rest;
+        int size = 1;
+        while (temp != null) {
+            size ++;
+            temp = temp.rest;
+        }
+        return size;
+    }
+
+    public int get(int i) {
+        if (i == 0) {
+            return first;
+        }
+        return  rest.get(i - 1);
+    }
+
+    public int iterativeGet(int i) {
+        if (i >= size() || i < 0) {
+            throw new RuntimeException("index is not correct!");
+        }
+        IntList temp = this;
+        while (i > 0) {
+            if (temp != null) {
+                temp = temp.rest;
+                i--;
+            } else {
+                throw new RuntimeException("index is not correct!");
+            }
+        }
+        return temp.first;
+    }
+}
